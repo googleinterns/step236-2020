5f4e465b6c4618cd7fb7ee53e0b00b6a0a287c09
Antonia <bocaantonia@gmail.com>
2020-08-24 16:31:22 +0300

    add top bar

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -2,22 +2,27 @@ import React from 'react';
 import UsersTable from './UsersTable';
 import ActionTable from './ActionTable';
 import PendingTable from './PendingTable';
+import AdminTopBar from './AdminTopBar';
 import styles from './admin.module.css';
+
 
 //class that handles the front page of the admin interface
 
 export default function AdminFrontPage() {
   return (
-    <div className={styles.adminLayout}>
-      <div className={styles.tableContainer}>
-        <UsersTable />
-      </div>
-      <div className={styles.actionContainer}>
+    <div>
+      <AdminTopBar />
+      <div className={styles.adminLayout}>
         <div className={styles.tableContainer}>
-          <ActionTable />
+          <UsersTable />
         </div>
-        <div className={styles.tableContainer}>
-          <PendingTable />
+        <div className={styles.actionContainer}>
+          <div className={styles.tableContainer}>
+            <ActionTable />
+          </div>
+          <div className={styles.tableContainer}>
+            <PendingTable />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/admin/AdminTopBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import styles from './admin.module.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1d3557",
+    },
+    secondary: {
+      main: "#457b9d",
+    }
+  }
+});
+
+export default function AdminTopBar() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppBar className={styles.topBar} position="static">
+        <Toolbar>
+          <Typography variant="h6" className={styles.header}>
+            Spooglers.org admin
+          </Typography>
+          <Button color="inherit">Log out</Button>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
