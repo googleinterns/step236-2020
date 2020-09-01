4198f20a74ab446acaa39da48c459d10a66dbb9a
Antonia Boca <bocaantonia@gmail.com>
2020-09-01 22:28:09 +0300

    Add firebase config to project (#23)
    * add firebase config to project
    
    * add new line at the end of firestore.rules
    
    Co-authored-by: Alicja Kluczek <alicja2602@gmail.com>
    
    * Update src/index.js with google auth provider
    
    Co-authored-by: Alicja Kluczek <alicja2602@gmail.com>
    
    * add file with firebase objects
    
    * add missing import
    
    Co-authored-by: Alicja Kluczek <alicja2602@gmail.com>

---
+++ src/firebaseFeatures.js
@@ -0,0 +1,23 @@
+// @flow
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyByClHaWsJw2jVjIlTQ2FIzaeI6hs0Y7tk',
+  authDomain: 'scriba-1d195.firebaseapp.com',
+  databaseURL: 'https://scriba-1d195.firebaseio.com',
+  projectId: 'scriba-1d195',
+  storageBucket: 'scriba-1d195.appspot.com',
+  messagingSenderId: '627308594363',
+  appId: '1:627308594363:web:f4f6a12dc387f8e196559e',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+const signInWithGoogle = (): void => auth.signInWithPopup(provider);
+
+export {firebase, auth, signInWithGoogle};
